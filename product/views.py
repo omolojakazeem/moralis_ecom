a68935f58c35bb55eacfe70f4842e878bdbe6cca
Omoloja Kazeem <omolojakazeem@gmail.com>
2021-11-06 22:19:55 +0100

    Added product

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from . import models
 
-# Create your views here.
+
+class Index(View):
+    template = 'product/index.html'
+
+    def get(self, request):
+        products = models.Product.objects.all()
+        context = {
+            'products': products
+        }
+        return render(request, template_name=self.template, context=context)
